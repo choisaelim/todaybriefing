80c2bb0a7db397f4d84151bdea498f08fcf7a384
choisaelim <choisaelim@gmail.com>
2023-01-13 23:45:05 +0900

    인증 필터 추가

--- user/src/main/java/com/example/user/security/SecurityConfig.java
+++ user/src/main/java/com/example/user/security/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.example.user.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,22 +14,31 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.user.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final Environment env;
+    private final AuthenticationManager authenticationManager;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .access("hasIpAddress('127.0.0.1')")
+                .and().addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
         return http.build();
 
     }
 
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    // {
-    // auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-    // }
-}// Using generated security password: 596adc3b-4938-4b0c-9cc3-b5cf4330cafb
+    private AuthenticationFilter getAuthenticationFilter() {
+        AuthenticationFilter authFilter = new AuthenticationFilter();
+        authFilter.setAuthenticationManager(authenticationManager);
+
+        return authFilter;
+    }
+
+}
